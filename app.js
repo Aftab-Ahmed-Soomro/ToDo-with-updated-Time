9818bc41713fef846276a34022d1ead0bb09a32d
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2024-05-18 17:35:08 -0700

    updated files

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ function myToDo() {
     let input = document.getElementById('input');
     let task = document.getElementById('task');
     let time = document.getElementById('time');
+    let copyright = document.getElementById('copyright');
+    let error = document.getElementById('error');
 
     let now = new Date();
 
     if (input.value.trim() == '') {
-        Swal.fire({
-            title: "Alert!",
-            text: "Please input the required field!",
-            icon: "warning",
-          })
+        error.innerText = 'Please input the required field!';
     }
     else {
         task.innerText += '⭕' + input.value + '\n\n';
         time.innerText += `${now.getDate()}/${now.getMonth()}/${now.getFullYear()} ${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}\n\n`;
+        input.value = '';
+        error.innerText = '';
     }
+    copyright.innerText = now.getFullYear();
 }
